0fbe05ba298d74cadfe6b940ad6bc7957eb9adfd
FlorianAGBO <fayemi2005@gmail.com>
2025-06-01 12:39:22 +0200

    Ajout du rapport final ProjetPython.pdf et nettoyage ancien rapport

--- src/controller/reservation_controller.py
+++ src/controller/reservation_controller.py
@@ -2,19 +2,38 @@ from model.client import Client
 from model.salle import Salle
 from model.reservation import Reservation
 from utils.json_handler import load_data, save_data
+from datetime import datetime
 
 class ReservationController:
     def __init__(self):
         data = load_data()
         self.clients = [Client(**c) for c in data.get("clients", [])]
         self.salles = [Salle(**s) for s in data.get("salles", [])]
-        self.reservations = [Reservation(**r) for r in data.get("reservations", [])]
+        self.reservations = [
+            Reservation(
+                r["client_id"],
+                r["salle_id"],
+                datetime.strptime(r["date_debut"], "%Y-%m-%d %H:%M"),
+                datetime.strptime(r["date_fin"], "%Y-%m-%d %H:%M"),
+                id=r.get("id")
+            )
+            for r in data.get("reservations", [])
+        ]
 
     def save(self):
         data = {
             "clients": [vars(c) for c in self.clients],
             "salles": [vars(s) for s in self.salles],
-            "reservations": [vars(r) for r in self.reservations]
+            "reservations": [
+                {
+                    "id": r.id,
+                    "client_id": r.client_id,
+                    "salle_id": r.salle_id,
+                    "date_debut": r.date_debut.strftime("%Y-%m-%d %H:%M") if isinstance(r.date_debut, datetime) else r.date_debut,
+                    "date_fin": r.date_fin.strftime("%Y-%m-%d %H:%M") if isinstance(r.date_fin, datetime) else r.date_fin
+                }
+                for r in self.reservations
+            ]
         }
         save_data(data)
 
